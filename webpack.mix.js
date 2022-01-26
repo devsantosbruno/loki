b307825ff878b7b164e1316fe90d180bde73a213
bruno <devbrunosantos@gmail.com>
2022-01-26 11:21:24 -0300

    fix: corrigindo compilacao sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,3 +3,13 @@
 let mix = require('laravel-mix')
 
 mix.js('src/app.js', 'dist').setPublicPath('dist')
+mix.sass('src/style.scss', 'dist').setPublicPath('dist')
+
+mix.webpackConfig({
+  resolve: {
+    modules: ['node_modules'],
+    alias: {
+      jquery: 'jquery/src/jquery'
+    }
+  }
+})
